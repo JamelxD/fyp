d55add6da860c5502f61875a77fe277c411e4c3a
JamelxD <akjamesworks@hotmail.co.uk>
2019-11-25 20:16:48 +0000

    Added location, get lat and long not just json object to later be used with an Api for borough

--- App.tsx
+++ App.tsx
@@ -27,9 +27,12 @@ export default class App extends Component {
   }
 
   state = {
-    location: null,
     one: null,
     two: null,
+    location: {
+      latitude: 0,
+      longitude: 0,
+    },
   }
   anotherstate = {
     one: null,
@@ -45,9 +48,15 @@ export default class App extends Component {
   findCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        const location = JSON.stringify(position);
+        const location = JSON.parse(JSON.stringify(position));
+        const locationObj = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        };
 
-        this.setState({ location });
+        this.setState({
+          location: locationObj 
+        });
       },
       error => alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
@@ -61,7 +70,7 @@ export default class App extends Component {
           title = 'Test'
           onPress = {this.findCoordinates}
         />
-        <Text>Location: {this.state.location}</Text>
+        <Text>Location: {this.state.location.latitude}, {this.state.location.longitude}</Text>
         <TextInput 
           keyboardType = 'numeric'
           onChangeText = {(e) => this.setState({'one': e})}
