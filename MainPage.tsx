924f0a2ccb34eb0cbb17f86e3eee51d6fdbc79c6
JamelxD <akjamesworks@hotmail.co.uk>
2020-02-29 16:59:56 +0000

    Implementation of map and custom markers for disabled location / car parks

--- MainPage.tsx
+++ MainPage.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage, Dimensions } from 'react-native';
 import Routes from './Routes';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class MainPage extends Component {
     state = {
@@ -13,6 +14,22 @@ export default class MainPage extends Component {
                 N: null,
             },
         },
+        markers: [
+            {
+                key: 0,
+                lat: 51.514226,
+                long: -0.109378,
+                title: 'Test',
+                description: 'Test'
+            },
+            {
+                key: 1,
+                lat: 51.514626,
+                long: -0.109378,
+                title: 'Test',
+                description: 'Test'
+            }
+        ],
     }
 
     constructor(props) {
@@ -23,7 +40,7 @@ export default class MainPage extends Component {
     async getBorough() {
         const borough = await AsyncStorage.getItem('location-borough');
         this.setState({
-            boroughName: borough
+            boroughName: borough,
         });
 
         this.getBoroughInfo();
@@ -31,28 +48,50 @@ export default class MainPage extends Component {
 
     async getBoroughInfo() {
         Routes.getBoroughInfo(this.state.boroughName)
-        .then(response => {
-            this.setState({
-                data: response.data,
+            .then(response => {
+                this.setState({
+                    data: response.data,
+                });
             });
-        });
     }
 
     render() {
+        console.log(this.state);
         return (
             <View style={styles.container}>
-                <Text>{this.state.boroughName}</Text>
+                {/* <Text>{this.state.boroughName}</Text>
                 <Text>Disabled Badge Parking Limit: {this.state.data.disabled_badge_parking_limit.N}</Text>
-                <Text>PCN Prices: {this.state.data.pcn_prices.N}</Text>
+                <Text>PCN Prices: {this.state.data.pcn_prices.N}</Text> */}
+                <MapView
+                    provider={'google'}
+                    style={styles.mapStyle}
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    showsMyLocationButton={true}
+                    minZoomLevel={16}
+                    loadingEnabled={true}
+                >
+                    {this.state.markers.map(marker => (
+                    <Marker
+                        coordinate={{latitude: marker.lat, longitude: marker.long}}
+                        title={marker.title}
+                        description={marker.description}
+                    />
+                    ))}
+                </MapView>
             </View>
         );
     }
 }
-  
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
 });
